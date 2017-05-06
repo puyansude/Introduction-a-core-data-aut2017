6f88cfb23adabc972ab1e302303be25ff433b8a2
Alain <alain@macbook-pro.local>
2017-05-06 15:36:13 -0400

    Départ

--- Etape01/ViewController.swift
+++ Etape01/ViewController.swift
@@ -1,25 +1,110 @@
-//
 //  ViewController.swift
 //  Etape01
-//
-//  Created by Alain on 17-05-06.
-//  Copyright © 2017 Alain. All rights reserved.
-//
+//  ------------------------------------------
+//  Contenu:
+// 
+//  1 - Utilisation du pointeur vers le dataStore du projet
+//  2 - Obtenir le contexte de la BD
+//  3 - Création d'un objet de type 'classe entité'
+//  4 - Renseigner les propriétés du l'objet
+//  5 - Enregistrer l'objet dans la BD
+//  6 - Obtenir tous les objets d'une entité puis les afficher
 
 import UIKit
 
+// MARK: contenu 1
+import CoreData
+
 class ViewController: UIViewController {
+
+    // MARK: contenu 1
+    var contexteDeLaBD: NSManagedObjectContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        // MARK: contenu 2 - Obtenir le contexte de la BD
+        // Le contexte de la BD est disponible dans la classe 'AppDelegate.swift' du projet
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate  {
+            contexteDeLaBD =  appDelegate.persistentContainer.viewContext
+        } else
+            { print("Erreur:  Impossible d'obtenir le contexte de la BD")
+                return
+        } // if let
+
+        print("👻: ", contexteDeLaBD)
+        
+        //  MARK: 3 - Création d'un objet 'Cours' de type 'classe entité'
+        //  Note: Xcode va automatiquement générer des classes
+        //  à partir des entités du schéma de la BD du projet.
+        let unCours = Cours(context: contexteDeLaBD)
+
+        // MARK: 4 - Renseigner les propriétés de l'objet
+        unCours.numero  = "📚 482-ICD-1"
+        unCours.nom     = "Introduction à core data niveau 1"
+
+        // MARK: 5 - Enregistrer l'objet dans la BD
+        do  {
+                try contexteDeLaBD.save()
+            }
+        catch
+                let error as NSError
+            {
+                print("Impossible d'enregistrer les données. \(error), \(error.userInfo)")
+            }
+
+        // MARK: 6 - Obtenir tous les objets d'une entité puis les afficher
+        // 6.1 - créer une requête d'intérogation
+        let fetchRequest:NSFetchRequest<Cours> = Cours.fetchRequest()
+        // 6.2 - exécuter la requête. Par defaut, la requete est 'select * from'
+        do {
+            // Création d'un tableau de cours, [Cours]
+            let lesCours = try contexteDeLaBD.fetch(fetchRequest)
+           
+            // 6.3 - Afficher les cours
+            for cours in lesCours {
+                print (cours.numero!, cours.nom!)
+            }
+ 
+        } catch let error as NSError {
+            print("Impossible de chercher les données. \(error), \(error.userInfo)")
+        }
+
+        // Ajouter des cours via une méthode
+        ajouterDesCours(5)
+        let lesCours = try! contexteDeLaBD.fetch(fetchRequest)
+        
+        // Afficher les cours
+        print("----------------------------------------------")
+        for cours in lesCours {
+            print (cours.numero!, cours.nom!)
+        }
+
+        
+    } // viewDidLoad
 
 
-}
+} // class ViewController
 
+extension ViewController {
+
+    func ajouterDesCours(_ nombre:Int){
+        for i in 2...nombre + 1 {
+            // print("i = \(i)")
+            // Créer un cours
+            let unCours = Cours(context: contexteDeLaBD)
+            unCours.numero  = "📕 482-ICD-\(i)"
+            unCours.nom     = "Introduction à core data niveau \(i)"
+            
+            // Enregistrer l'objet dans la BD
+            do  {
+                try contexteDeLaBD.save()
+            }
+            catch
+                let error as NSError
+            {
+                print("Impossible d'enregistrer les données. \(error), \(error.userInfo)")
+            }
+        } // for
+    } // ajouterDesCours
+} // extension ViewController
